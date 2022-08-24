1f1306e84b9fc6b993db49328158f10af17df02d
pihujindal <110271241+pihujindal@users.noreply.github.com>
2022-08-24 11:52:45 +0530

    Update rolldice.py

--- rolldice.py
+++ rolldice.py
@@ -4,7 +4,42 @@ def rolldice(min,max):
             print('rolling the dice....')
             no=random.randint(min,max)
             print(f'your number:{no}')
+                      
+            if no==1:
+                print('[------]')
+                print('[------]')
+                print('[   0  ]')
+                print('[------]')
+                print('[------]')
+            elif no==2:
+                print('[------]')
+                print('[      ]')  
+                print('[  0 0 ]')
+                print('[      ]')   
+                print('[------]')
+            elif no==3:
+                print('[------]')
+                print('[      ]')
+                print('[ 0 0 0]')
+                print('[      ]')
+                print('[------]')
+            elif no==4:
+                print('[------]')
+                print('[ 0  0 ]')
+                print('[      ]')
+                print('[ 0  0 ]') 
+                print('[------]')
+            else:
+                 no==5
+                 print('[------]')
+                 print('[0    0]')
+                 print('[   0  ]')
+                 print('[0    0]')
+                 print('[------]')
             choice= input('Do you want to  roll dice again?(y/n)')
-            if choice.lower()=='n':
+            if choice.lower()=='y':
+                continue
+            else:
                 break
+
 rolldice(1,6)
